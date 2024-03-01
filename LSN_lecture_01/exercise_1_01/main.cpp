866835af537a5c3807b642918f89310c44b7142c
francescogrienti <francesco.grienti@studenti.unimi.it>
2024-03-01 16:11:53 +0100

    First exercise, first commit

--- LSN_lecture_01/exercise_1_01/main.cpp
+++ LSN_lecture_01/exercise_1_01/main.cpp
@@ -11,6 +11,8 @@ _/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cmath>
+#include <cstdlib>
 #include "random.h"
 
 using namespace std;
@@ -18,7 +20,10 @@ using namespace std;
 int main (int argc, char *argv[]){
 
    Random rnd;
-   int N = 30;
+   int M = 100000; //Number of total throws
+   int N = 100; //Number of blocks
+   int L = M/N; //Number of throws per block
+   double ave[N], ave2[N]; //Arrays for storing the average and the square of the average for each block
    int seed[4];
    int p1, p2;
    ifstream Primes("Primes");
@@ -41,9 +46,14 @@ int main (int argc, char *argv[]){
    } else cerr << "PROBLEM: Unable to open seed.in" << endl;
 
    for(int i=0; i<N; i++){
-      cout << rnd.Rannyu() << endl;
+       double sum = 0.;
+       for(int j=0; j<L; j++){
+           double r = rnd.Rannyu();
+           sum += r;
+       }
+       ave[i] = double(sum/L);
+       ave2[i] = double(pow(ave[i], 2));
    }
-
    rnd.SaveSeed();
    return 0;
 }
